075f453697e5651d7b5af2b017b49273619fccc6
shu-ya318 <funghi0983524367@gmail.com>
2026-06-22 07:29:24 +0800

    build(docker,k8s): add backend admin password variable

--- sql-scripts/init-db.sh
+++ sql-scripts/init-db.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-# set -e
+set -e
 
 /opt/mssql/bin/sqlservr &
 
-echo "Waiting for SQL Server to start..."
 until /opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P "$MSSQL_SA_PASSWORD" -C -Q "SELECT 1" > /dev/null 2>&1; do
     sleep 2
 done
-echo "SQL Server is ready. Running initialization script..."
 
 /opt/mssql-tools18/bin/sqlcmd \
     -S localhost \
@@ -19,5 +17,4 @@ echo "SQL Server is ready. Running initialization script..."
     -v DB_PASSWORD="$DB_PASSWORD" \
     -i /sql-scripts/init.sql
 
-echo "Database initialization complete."
 wait
